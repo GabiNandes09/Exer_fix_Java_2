d6ffb10145109946180e7dbdff9ebb59195e2f7c
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2024-01-30 10:04:43 -0300

    add entities package

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,5 +1,70 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
 public class Order {
+	
+	private Date moment;
+	private OrderStatus status;
+	
+	private List<OrderItem> orderItem = new ArrayList <>();
+	private Client client;
+	
+	public Order() {
+	}
+
+	public Order(Date moment, OrderStatus status, List<OrderItem> orderItem, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public List<OrderItem> getOrderItem() {
+		return orderItem;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	public void addItem(OrderItem item) {
+		orderItem.add(item);
+	}
+	
+	public void removeItem(OrderItem item) {
+		orderItem.remove(item);
+	}
+	
+	public Double total() {
+		Double sum = 0.0;
+		for(OrderItem c: orderItem) {
+			sum += c.subTotal();
+		}
+		return sum;
+	}
 
 }
