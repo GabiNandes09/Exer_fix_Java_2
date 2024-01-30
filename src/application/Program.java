f9a010871c0730a944f44802de90b277803aed6c
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2024-01-30 10:28:33 -0300

    Add Main Program until Itens register in Order feature

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,55 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
+		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("DD/MM/YYYY");
+		
+		System.out.println("ENTER CLIENT DATA:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.next());
+		Client client = new Client(name, email, birthDate);
+		System.out.println();
+		
+		System.out.println("ENTER ORDER DATA:");
+		System.out.print("Status: ");
+		String status = sc.next();
+		System.out.println();
+		Order order = new Order(new Date(), OrderStatus.valueOf(status), client);
+		
+		System.out.print("How many items to this order? ");
+		int n = sc.nextInt();
+		sc.nextLine();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println("ENTER #" + i + "ITEM DATA:");
+			System.out.print("Product name:" );
+			String itemName = sc.nextLine();
+			System.out.print("Product price: ");
+			Double price = sc.nextDouble();
+			System.out.print("Quantity: ");
+			Integer quantity = sc.nextInt();
+			OrderItem item = new OrderItem(quantity, price, new Product(itemName, price));		
+			order.addItem(item);
+		}
+ 
 
 	}
 
